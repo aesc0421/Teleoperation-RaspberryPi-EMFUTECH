d912a5ee677b45fc4d089e2e01ceb9d3fbb10660
RicardoRam781 <alexrp782@gmail.com>
2025-04-23 12:01:05 +0900

    change direction in functions

--- excavator.py
+++ excavator.py
@@ -8,7 +8,7 @@ IN2 = DigitalOutputDevice(22)
 #Oruga izquierda
 PWM2 = PWMOutputDevice(12)
 IN2_1 = DigitalOutputDevice(21)
-IN2_2 = DigitalOutputDevice(11)
+IN2_2 = DigitalOutputDevice(9)
 #Oruga derecha
 PWM3 = PWMOutputDevice(13)
 IN3_1 = DigitalOutputDevice(5)
@@ -90,7 +90,7 @@ def stop_rigth():
 def stop_left():
             PWM2.off()
 
-def right(direction, value):
+def left(direction, value):
             value = abs(float(value))
             if direction == "forward":
                 IN3_1.off()
@@ -105,14 +105,17 @@ def right(direction, value):
             PWM3.value = value
             sleep(timeSleep)
 
-def left(direction, value):
+def right(direction, value):
             value = abs(float(value))
             if direction == "forward":
-                IN2_1.off()
-                IN2_2.on()
+                print("forward left")
+              
+                IN2_1.off()               
+                IN2_2.on()                
             elif direction == "backward":
+                print("bacward left")
                 IN2_1.on()
-                IN2_2.off()
+                IN2_2.off()                
             else:
                 IN2_1.off()
                 IN2_2.off()
@@ -132,13 +135,13 @@ def rotate_left():
             PWM5.value = generalSpeed
             sleep(timeSleep)
 
-def move_bucket_up():
+def move_bucket_down():
             PWM6_IN1_3.on()
             PWM6_IN2_3.off()
             PWM6.value = generalSpeed
             sleep(timeSleep)
 
-def move_bucket_down():
+def move_bucket_up():
             PWM6_IN1_3.off()
             PWM6_IN2_3.on()
             PWM6.value = generalSpeed
@@ -158,44 +161,45 @@ def move_axis2_down():
             PWM7.value = generalSpeed
             sleep(timeSleep)
 
-# try:
-#       while True:
-#         opcion = input("Ingresa dirección (1=arriba, 2=abajo, 3=adelante, 5=rotacion, 4=atras, 6=pala upm 7=pala down, arm2 up =8, arm2 down = 9 0=detener,): ")
+try:
+      while True:
+        opcion = input("Ingresa dirección (1=arriba, 2=abajo, 3=adelante, 5=rotacion, 4=atras, 6=pala upm 7=pala down, arm2 up =8, arm2 down = 9 0=detener,): ")
 
-#         if opcion == "1":
-#             PWM.value = 0.2
-#             move_axis1_Up()
-#         elif opcion == "2":
-#             PWM.value = 0.2
-#             move_axis1_Down()
-#         elif opcion == "0":
-#             Stop()
-#             stop_movement()
-#         elif opcion == "3":
-#             PWM2.value = 0.2
-#             right()
-#             left()
-#         elif opcion == "4":
-#             PWM3.value = 0.2
-#             back()
-#         elif opcion == "6":
-#             move_bucket_up()
-#         elif opcion == "7":
-#             move_bucket_down()
-#         elif opcion == "8":
-#             move_axis2_down()
-#         elif opcion == "9":
-#             move_axis2_up()
-#         elif opcion == "5":
-#             rotate_right()
-#         elif opcion == "a":
-#             rotate_left()
-#         else:
-#             print("1, 2 o 0.")
-#         sleep(0.1)
-# except KeyboardInterrupt:
-#     print("Apagando motores...")
-#     for p in [PWM, PWM2, PWM3]:
-#         p.off()
-#     for pin in [IN1, IN2, IN2_1, IN2_2, IN3_1, IN3_2]:
-#         pin.off()
+        if opcion == "1":
+            PWM.value = 0.2
+            move_axis1_Up()
+        elif opcion == "2":
+            PWM.value = 0.2
+            move_axis1_Down()
+        elif opcion == "0":
+            Stop()
+            stop_movement()
+        elif opcion == "3":
+            #right('forward',1)
+          
+            left('backward',1)
+        elif opcion == "4":
+            left('forward',1)
+            #right('backward',1)
+          
+        elif opcion == "6":
+            move_bucket_up()
+        elif opcion == "7":
+            move_bucket_down()
+        elif opcion == "8":
+            move_axis2_down()
+        elif opcion == "9":
+            move_axis2_up()
+        elif opcion == "5":
+            rotate_right()
+        elif opcion == "a":
+            rotate_left()
+        else:
+            print("1, 2 o 0.")
+        sleep(0.1)
+except KeyboardInterrupt:
+    print("Apagando motores...")
+    for p in [PWM, PWM2, PWM3]:
+        p.off()
+    for pin in [IN1, IN2, IN2_1, IN2_2, IN3_1, IN3_2]:
+        pin.off()
